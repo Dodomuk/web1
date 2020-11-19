b5dbbb8db1b4088a4eafac90c1f7140ed839644e
노력형 개발자 <64397635+Dodomuk@users.noreply.github.com>
2020-11-19 15:35:02 +0900

    [백준]평균은넘겠지
    입력 : 첫째 줄에는 테스트 케이스의 개수 C가 주어진다.
    
    둘째 줄부터 각 테스트 케이스마다 학생의 수 N(1 ≤ N ≤ 1000, N은 정수)이 첫 수로 주어지고, 이어서 N명의 점수가 주어진다. 점수는 0보다 크거나 같고, 100보다 작거나 같은 정수이다.
    
    출력 : 각 케이스마다 한 줄씩 평균을 넘는 학생들의 비율을 반올림하여 소수점 셋째 자리까지 출력한다.

---
+++ OverAverage.java
@@ -0,0 +1,75 @@
+package a_oneDimensionalArray;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class OverAverage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		BufferedReader br = new BufferedReader
+				(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		try {
+			
+			StringTokenizer st;
+			
+			int c = Integer.parseInt(br.readLine()); // 입력 받을 테스트 케이스의 개수
+			int[] cArr = new int[c]; // 테스트 케이스만큼 점수를 입력하기 위한 배열
+
+			for (int i = 0; i < cArr.length; i++) {
+				
+			    int overAvg = 0; //평균을 넘는 학생의 수
+
+				st = new StringTokenizer(br.readLine()," ");
+				
+				cArr[i] = Integer.parseInt(st.nextToken());//학생들
+				
+		
+				
+				int[] nArr = new int[cArr[i]]; // 학생의 점수들
+				int sum = 0; // 점수 합계
+				
+				for (int j = 0; j < nArr.length; j++) {
+					nArr[j] = Integer.parseInt(st.nextToken());
+					sum += nArr[j];
+
+				}
+				
+				double avg = sum/nArr.length; //평균 점수 산출
+				
+				for (int k = 0; k < nArr.length; k++) { //점수 평균을 넘는 갯수를 구하기 위한 for문 
+		
+					if(nArr[k] > avg) {					
+						overAvg ++;
+								
+				}
+				}
+				System.out.println(overAvg);
+				System.out.println(nArr.length);
+				double overAvgs = ((double)overAvg / (nArr.length))*100;
+				System.out.println(overAvgs);
+	         			//소수점 3자리 표현식
+						sb.append(overAvgs + "%");	
+						sb.append("\n");				
+			}
+			
+			System.out.println(sb);
+			
+		} catch (NumberFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+	}
+
+}
