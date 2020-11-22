4c2a88a6be4f66658d069a99a82ace7156349972
노력형 개발자 <64397635+Dodomuk@users.noreply.github.com>
2020-11-22 15:24:22 +0900

    Add files via upload
    정수 n개가 주어졌을 때, n개의 합을 구하는 함수를 작성하시오.
    
    작성해야 하는 함수는 다음과 같다.
    
    Java: long sum(int[] a); (클래스 이름: Test)
    a: 합을 구해야 하는 정수 n개가 저장되어 있는 배열 (0 ≤ a[i] ≤ 1,000,000, 1 ≤ n ≤ 3,000,000)
    리턴값: a에 포함되어 있는 정수 n개의 합

---
+++ Test.java
@@ -0,0 +1,19 @@
+package f_function;
+
+public class Test {
+
+		long sum(int[] a) {
+			
+			long ans = 0;
+			
+			for (int n = 0; n < a.length; n++) {
+			    
+			  ans += a[n] ;
+			    
+			}
+			
+			return ans;
+		
+		}		
+
+}
