8af207494f7240f958fdec6297957097af5be946
노력형 개발자 <64397635+Dodomuk@users.noreply.github.com>
2020-11-23 22:03:44 +0900

    [백준]아스키코드
    알파벳 소문자, 대문자, 숫자 0-9중 하나가 주어졌을 때, 주어진 글자의 아스키 코드값을 출력하는 프로그램을 작성하시오.

---
+++ ASCII_code.java
@@ -0,0 +1,21 @@
+package g_string;
+
+import java.util.Scanner;
+
+public class ASCII_code {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		
+		  char input = sc.next().charAt(0);
+		  
+		  int ascllcode = (int)input;		 
+
+		System.out.println(ascllcode);
+		
+	}
+	
+	
+}
