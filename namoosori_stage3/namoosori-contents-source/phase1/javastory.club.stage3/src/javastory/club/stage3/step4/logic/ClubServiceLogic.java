d3264fe85f0fdfd7c4b3d0c205f67bed42d8f7ee
MUk DoDo <siderid94@gmail.com>
2021-06-17 23:34:15 +0900

    git commit

--- namoosori_stage3/namoosori-contents-source/phase1/javastory.club.stage3/src/javastory/club/stage3/step4/logic/ClubServiceLogic.java
+++ namoosori_stage3/namoosori-contents-source/phase1/javastory.club.stage3/src/javastory/club/stage3/step4/logic/ClubServiceLogic.java
@@ -20,7 +20,7 @@ import javastory.club.stage3.step4.util.NoSuchMemberException;
 import javastory.club.stage3.util.StringUtil;
 import javastory.club.stage3.step4.da.map.ClubStoreMapLycler;
 
-public class ClubServiceLogic implements ClubService{
+public class ClubServiceLogic implements ClubService {
 
     private ClubStore clubStore;
     private MemberStore memberStore;
@@ -34,7 +34,9 @@ public class ClubServiceLogic implements ClubService{
     public void register(TravelClubDto clubDto) {
 
         Optional.ofNullable(clubStore.retrieveByName(clubDto.getName()))
-                .ifPresent(travelClub -> { throw new ClubDuplicationException("이미 존재하는 이름입니당");});
+                .ifPresent(travelClub -> {
+                    throw new ClubDuplicationException("이미 존재하는 이름입니당");
+                });
 
         TravelClub club = clubDto.toTravelClub();
         String clubId = clubStore.create(club);
@@ -45,7 +47,7 @@ public class ClubServiceLogic implements ClubService{
     @Override
     public TravelClubDto findClub(String clubId) {
 
-       return Optional.ofNullable(clubStore.retrieve(clubId))
+        return Optional.ofNullable(clubStore.retrieve(clubId))
                 .map(travelClub -> new TravelClubDto(travelClub))
                 .orElseThrow(() -> new NoSuchClubException("해당 아이디를 가진 클럽이 존재하지 않습니다. "));
 
@@ -61,15 +63,17 @@ public class ClubServiceLogic implements ClubService{
     @Override
     public void modify(TravelClubDto clubDto) {
         Optional.ofNullable(clubStore.retrieveByName(clubDto.getName()))
-                .ifPresent(travelClub -> {throw new ClubDuplicationException("해당 이름의 클럽이 이미 존재합니다.");});
+                .ifPresent(travelClub -> {
+                    throw new ClubDuplicationException("해당 이름의 클럽이 이미 존재합니다.");
+                });
 
         TravelClub club = Optional.ofNullable(clubStore.retrieve(clubDto.getUsid()))
                 .orElseThrow(() -> new NoSuchClubException("해당 아이디를 가진 클럽이 존재하지 않습니다 . "));
 
-        if(clubDto.getName().isEmpty() || clubDto.getName() == null){
+        if (clubDto.getName().isEmpty() || clubDto.getName() == null) {
             clubDto.setName(club.getName());
         }
-        if(clubDto.getIntro().isEmpty() || clubDto.getIntro() == null){
+        if (clubDto.getIntro().isEmpty() || clubDto.getIntro() == null) {
             clubDto.setIntro(club.getIntro());
         }
 
@@ -80,7 +84,7 @@ public class ClubServiceLogic implements ClubService{
     @Override
     public void remove(String clubId) {
 
-        if(!clubStore.exists(clubId)){
+        if (!clubStore.exists(clubId)) {
             throw new NoSuchClubException("해당 아이디를 가진 클럽이 존재하지 않습니다.");
         }
 
@@ -91,25 +95,84 @@ public class ClubServiceLogic implements ClubService{
     @Override
     public void addMembership(ClubMembershipDto membershipDto) {
 
+        String memberId = membershipDto.getMemberEmail();
+
+        CommunityMember member = Optional.ofNullable(memberStore.retrieve(memberId))
+                .orElseThrow(() -> new NoSuchMemberException("멤버가 존재하지 않습니다."));
+
+        TravelClub club = clubStore.retrieve(membershipDto.getClubId());
+        for (ClubMembership clubMembership : club.getMembershipList()) {
+            if(memberId.equals(clubMembership.getMemberEmail())){
+                throw new MemberDuplicationException("멤버가 이미 존재합니다.");
+            }
+        }
+
+        ClubMembership clubMembership = membershipDto.toMembership();
+        club.getMembershipList().add(clubMembership);
+
     }
 
     @Override
     public ClubMembershipDto findMembershipIn(String clubId, String memberId) {
-        return null;
+        TravelClub club = clubStore.retrieve(clubId);
+        ClubMembership membership = getMembershipIn(club,memberId);
+
+        return new ClubMembershipDto(membership);
     }
+
 
     @Override
     public List<ClubMembershipDto> findAllMembershipsIn(String clubId) {
-        return null;
+
+        TravelClub club = clubStore.retrieve(clubId);
+        return club.getMembershipList().stream()
+                .map(clubMembership -> new ClubMembershipDto(clubMembership))
+                .collect(Collectors.toList());
+
     }
 
     @Override
     public void modifyMembership(String clubId, ClubMembershipDto member) {
 
+        String targetEmail = member.getMemberEmail();
+        RoleInClub newRole = member.getRole();
+
+        TravelClub targetClub = clubStore.retrieve(clubId);
+        ClubMembership membership = getMembershipIn(targetClub,targetEmail);
+        membership.setRole(newRole);
+        clubStore.update(targetClub);
+
+        CommunityMember targetMember = memberStore.retrieve(targetEmail);
+
+        targetMember.getMembershipList().stream().forEach(clubMembership ->
+        {
+            if(clubMembership.getClubId().equals(clubId)){
+                clubMembership.setRole(newRole);
+            }
+        });
+
+        memberStore.update(targetMember);
     }
 
     @Override
     public void removeMembership(String clubId, String memberId) {
 
+        TravelClub targetClub = clubStore.retrieve(clubId);
+        CommunityMember foundMember = memberStore.retrieve(memberId);
+        ClubMembership clubMembership = getMembershipIn(targetClub, foundMember.getEmail());
+
+        targetClub.getMembershipList().remove(clubMembership);
+        foundMember.getMembershipList().remove(clubMembership);
+
+    }
+
+    private ClubMembership getMembershipIn(TravelClub club, String memberEmail){
+
+        for (ClubMembership clubMembership : club.getMembershipList()) {
+                if(memberEmail.equals(clubMembership.getMemberEmail())){
+                    return membership;
+                }
+        }
+         throw new NoSuchMemberException("해당 멤버는 클럽에 존재하지 않습니다.");
     }
 }
